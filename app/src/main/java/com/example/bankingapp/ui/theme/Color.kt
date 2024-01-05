77a05dd3201cb2d3479c60075e1d9b77c6aba081
texnariO <texnari0@protonmail.com>
2024-01-05 14:21:49 +0100

    Add start project with simple Splash Screen

---
+++ app/src/main/java/com/example/bankingapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.bankingapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MainColorBlue = Color(0xFF3e8ce6)
+val MainColorGray = Color(0xFF6e6868)
---
+++ app/src/main/java/com/example/bankingapp/ui/theme/Type.kt
@@ -0,0 +1,55 @@
+package com.example.bankingapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+import com.example.bankingapp.R
+
+
+val MontserratFamily = FontFamily(
+        Font(R.font.montserrat_regular),
+        Font(R.font.montserrat_bold),
+        Font(R.font.montserrat_semibold)
+)
+// Set of Material typography styles to start with
+val Typography = Typography(
+
+        bodyLarge = TextStyle(
+                fontFamily = MontserratFamily,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = 0.5.sp,
+                fontSize = 20.sp
+        ),
+        bodyMedium = TextStyle(
+                fontFamily = MontserratFamily,
+                fontWeight = FontWeight.SemiBold,
+                letterSpacing = 0.5.sp,
+                fontSize = 20.sp
+                ),
+        bodySmall = TextStyle(
+                fontFamily = MontserratFamily,
+                fontWeight =  FontWeight.Normal,
+                letterSpacing = 0.5.sp,
+                fontSize = 20.sp
+        )
+        /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
